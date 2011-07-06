6f5f4c10dbd0ade1211fd3626ec924f2a0642e80
Guto Maia <guto@guto.net>
2011-07-06 20:22:36 -0300

    demo.php now entirely covered in tests

--- SimpleStringTest.php
+++ SimpleStringTest.php
@@ -9,8 +9,17 @@ class SimpleStringTest extends PHPUnit_Framework_TestCase {
 		$string = new SimpleString('Lorem ipsum dolor sit amet lorem ipsum');
 		$string->shorten(10);
 		$this->assertEquals('Lorem ipsu', $string->__toString());
+		$string->toSentenceCase();
+		$this->assertEquals('Lorem ipsu', $string->__toString());
 		
-
+		$string = new SimpleString('Lorem ipsum dolor sit amet lorem ipsum');
+		$string->shorten(15);
+		$this->assertEquals('Lorem ipsum dol', $string->__toString());
+		$string = new SimpleString('Lorem ipsum dolor sit amet lorem ipsum');
+		$string->tolower();
+		$this->assertEquals('lorem ipsum dolor sit amet lorem ipsum', $string->__toString());
+		$string->replace('lorem', 'mortem');
+		$this->assertEquals('mortem ipsum dolor sit amet mortem ipsum', $string->__toString());
 	}
 
 
