903af7e4c64ca895cbffa0feab6ab56267913375
Thiago Alvarenga Lechuga <thiagoalz@gmail.com>
2011-07-08 11:14:57 -0300

    Improving demo test

--- tests/SimpleStringTest.php
+++ tests/SimpleStringTest.php
@@ -3,20 +3,25 @@ require_once 'SimpleString.class.php';
 
 class SimpleStringTest extends PHPUnit_Framework_TestCase {
 
+
 	public function testDemo(){
+		//Cumulative changes
 		$string = new SimpleString('Lorem ipsum dolor sit amet lorem ipsum');
 		$string->shorten(10);
 		$this->assertEquals('Lorem ipsu', $string->__toString());
 		$string->toSentenceCase();
 		$this->assertEquals('Lorem ipsu', $string->__toString());
 		
+		//Using the returned new SimpleString
 		$string = new SimpleString('Lorem ipsum dolor sit amet lorem ipsum');
-		$string->shorten(15);
+		$string=$string->shorten(15);
 		$this->assertEquals('Lorem ipsum dol', $string->__toString());
+		$string=$string->toCamelCase();
+		$this->assertEquals('loremIpsumDol', $string->__toString());
+
+		//Overloaded functions
 		$string = new SimpleString('Lorem ipsum dolor sit amet lorem ipsum');
-		$string->tolower();
-		$this->assertEquals('lorem ipsum dolor sit amet lorem ipsum', $string->__toString());
-		$string->replace('lorem', 'mortem');
+		$string->tolower()->replace('lorem', 'mortem');
 		$this->assertEquals('mortem ipsum dolor sit amet mortem ipsum', $string->__toString());
 	}
 
